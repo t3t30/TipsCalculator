d3d3fc305bfffc47edc6ab232f8b5fb8e8a4a972
t3t30 <mateusbie6@gmail.com>
2024-12-14 23:29:04 -0300

    add biding for connect the layers

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -5,12 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tipscalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnClean.setOnClickListener {
+
+
+        }
 
 
     }
