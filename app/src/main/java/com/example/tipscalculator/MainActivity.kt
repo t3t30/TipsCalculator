b9a5828a0dd3eab0daa5d8dfdd3a0d4804af6b84
t3t30 <mateusbie6@gmail.com>
2024-12-15 22:32:40 -0300

    add functions

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tipscalculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,16 +39,43 @@ class MainActivity : AppCompatActivity() {
 
 
        binding.btnCalculate.setOnClickListener{
-           val totalconta: Float = binding.tieconta.text.toString().toFloat()
-           val totalpessoas:Int = binding.tiepessoas.text.toString().toInt()
 
-           val total = totalconta + (totalconta * percent/100)
-           val totalporpessoa = total / totalpessoas
+           val totalcontatemp = binding.tieconta.text
+           val totalpessoastemp = binding.tieconta.text
+           if (totalcontatemp?.isEmpty() == true || totalpessoastemp?.isEmpty() == true ){
+              Snackbar.make(binding.tieconta, "Preencha todos os campos", Snackbar.LENGTH_LONG)
+                  .show()
 
-                //biding every
+           } else{
 
+               val totalconta: Float = totalcontatemp.toString().toFloat()
+               val totalpessoas:Int = totalpessoastemp.toString().toInt()
+
+               val total = totalconta + (totalconta * percent/100)
+               val totalporpessoa = total / totalpessoas
+
+               binding.txtResult.text= "Valor por pessoa: $totalporpessoa"
+
+               }
+
+
+
+                // validar campo vazio
+
+     
 
        }
 
+        //clean button
+       binding.btnClean.setOnClickListener { 
+           binding.txtResult.text= ""
+           binding.tiepessoas.setText("")
+           binding.tieconta.setText("")
+           binding.rgOne.isChecked = false
+           binding.rgTwo.isChecked = false
+           binding.rgThree.isChecked = false
+
+           
+       }
     }
 }
